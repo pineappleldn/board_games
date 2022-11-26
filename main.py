75024ab799c5544b2051517706e550eef6497744
Lymareva Daria <daria.lymareva@gmail.com>
2022-11-27 02:49:30 +0300

    Скелет main

--- main.py
+++ main.py
@@ -5,4 +5,25 @@ from board import *
 from interface import *
 from objects import *
 
-turn = Turn()
+
+def main():
+    """Главная функция главного модуля"""
+    pg.init()
+
+    width = 900
+    height = 900
+    screen = pg.display.set_mode((width, height))
+
+    turn = Turn()
+    finished = False
+    print('Game started!')
+
+    while not finished:
+        finished = True
+
+    print('Game finished!')
+    pg.quit()
+
+
+if __name__ == "__main__":
+    main()

2afc85b5bafdca057088daf42d94601d4f1626d1
Lymareva Daria <daria.lymareva@gmail.com>
2022-11-27 02:51:37 +0300

    Clean up code

--- input.py
+++ input.py
@@ -3,3 +3,7 @@ def check_on_board(event):
     Проверяет попал ли пользователь на доску, возвращает координаты квадрата в доске
     """
     pass
+
+
+if __name__ == "__main__":
+    print("This module is not for direct call!")

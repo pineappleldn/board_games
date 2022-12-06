b190b0cfb694a2af52da85a9871ea30f4d7733cb
Viktor Novoseltsev <novoselcevviktor191@gmail.com>
2022-12-06 11:14:32 +0300

    added class Board huge refactors to the objects.py file a rework of the Chips class removed Turn class moved functions into the board class

--- objects.py
+++ objects.py
@@ -2,12 +2,17 @@ import pygame as pg
 
 
 class Chips:
-    def __init__(self, screen):
-        self.color = 'white'
-        self.screen = screen
-        self.x = 0
-        self.y = 0
-        self.r = 30
+    def __init__(self, x=0, y=0, r=30, color='white'):
+        """
+        initiates instance of a chip on the board
+        :param x: x-coordinate of the chip
+        :param y: y-coordinate of the chip
+        :param r: radius of the chip
+        """
+        self.color = color
+        self.x = x
+        self.y = y
+        self.r = r
 
     def turn_over(self):
         """
@@ -18,75 +23,68 @@ class Chips:
         else:
             self.color = 'white'
 
-    def draw(self):
+    def draw(self, screen):
         """
         Рисует фишку
         """
-        pg.draw.circle(self.screen, self.color, (self.x, self.y), self.r)
+        pg.draw.circle(screen, self.color, (self.x, self.y), self.r)
+
+    def get_color(self):
+        if self.color == 'white':
+            return 1
+        elif self.color == 'black':
+            return 2
 
 
 class Hints:
-    def __init__(self, screen):
-        self.screen = screen
-        self.x = 0
-        self.y = 0
-        self.r = 15
+    def __init__(self, x=0, y=0, r=15):
+        self.x = x
+        self.y = y
+        self.r = r
         self.color = (255, 0, 0, 200)
 
-    def draw(self):
+    def draw(self, screen):
         """
         Рисует хинт
         """
-        pg.draw.circle(self.screen, self.color, (self.x, self.y), self.r)
-
-
-class Turn:
-    def __init__(self):
-        self.state = 0
-        self.count = 0
-
-    def turn_turn(self):
-        """
-        передает ход от одного игрока к другому
-        """
-        if self.state == 0:
-            self.state = 1
-        else:
-            self.state = 0
+        pg.draw.circle(screen, self.color, (self.x, self.y), self.r)
 
 
 class Board:
-    def __init__(self, screen):
-        self.matrix = []
-        self.screen = screen
-        self.x = 0
-        self.y = 0
-        self.board_size = 0
-        self.square_size = 0
+    board_size: int
 
-    def draw_board(self, screen):
+    def __init__(self, matrix=None, x=0, y=0, board_size=664):
+        self.matrix = [[None]*8]*8
+        self.x = x
+        self.y = y
+        self.board_size = board_size
+        self.image = pg.image.load('board.jpg')
+        for i in range(8):
+            for j in range(8):
+                x = int(self.x + self.board_size * (2 * i + 1) / 16)
+                y = int(self.y + self.board_size * (2 * j + 1) / 16)
+                if matrix[i][j] == 1:
+                    self.matrix[i][j] = Chips(x, y, color='white')
+                elif matrix[i][j] == 2:
+                    self.matrix[i][j] = Chips(x, y, color='black')
+                elif matrix[i][j] == 3:
+                    self.matrix[i][j] = Hints(x, y)
+
+    # noinspection PyUnresolvedReferences
+    def draw(self, screen):
         """
         Функция рисует игровое поле.
         board_size - ширина и высота картинки игрового поля
         """
-        board = pg.image.load('board.jpg')
-        screen.blit(board, (screen.get_width() - self.board_size, (screen.get_height() - self.board_size) / 2))
+        screen.blit(self.image, (screen.get_width() - self.board_size, (screen.get_height() - self.board_size) / 2))
+        for i in range(8):
+            for j in range(8):
+                if self.matrix[i][j] is not None:
+                    self.matrix[i][j].draw(screen)
 
-    def draw_cheaps(self, screen):
-        for x in range(8):
-            for y in range(8):
-                chip = Chips(screen)
-                hint = Hints(screen)
-                chip.x = screen.get_width() - board_size + 83 * (2 * x + 1) / 2
-                chip.y = (screen.get_height() - board_size) / 2 + 83 * (2 * y + 1) / 2
-                hint.x = screen.get_width() - board_size + 83 * (2 * x + 1) / 2
-                hint.y = (screen.get_height() - board_size) / 2 + 83 * (2 * y + 1) / 2
-                if matrix[x][y] == 1:
-                    chip.color = 'white'
-                    chip.draw()
-                elif matrix[x][y] == 2:
-                    chip.color = 'black'
-                    chip.draw()
-                elif matrix[x][y] == 3:
-                    hint.draw()
+    def check_on_board(self, event):
+        if event.button == 1:
+            if self.x <= event.pos[0] <= self.x + self.board_size:
+                if self.y <= event.pos[1] <= self.y + self.board_size:
+                    return 8 * (event.pos[0] - self.x - 1) // self.board_size, 8 * (event.pos[1] - self.y - 1) // self.board_size
 
